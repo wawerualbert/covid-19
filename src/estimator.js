d8a4503c620cdd507f0bad6241781fca7e47c7d4
wawerualbert58 <wawerualbert58@gmail.com>
2020-04-13 09:16:36 -0400

    challeng1

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,28 @@
 const covid19ImpactEstimator = (data) => {
-  const input = data;
-  return {
-    data: input,
-    impact: {},
-    severeImpact: {}
-  };
-};
+const { reportedCases, timeToLapse, periodType, totalHospitalBeds } = data;
+const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = data.region;
+
+const estimate;
+ if(periodType === 'months') {
+   estimate = Math.ceil(timeToLapse * 30);
+ } else if (periodType === 'weeeks') {
+   estimate = timeToLapse * 7;
+ } else {
+   estimate = timeToLapse;
+ };
+
+const factor = Math.trunc(estimate / 3);
+const results = 2 ** factor;
+
+const impact =  {};
+const severeimpact = {};
+
+const noOfCases = (reportedCases, estimatedNumber) => reportedCases * estimatedNumber;
+
+impact.currentlyInfected = noOfCases(reportedCases ,10);
+impact.infectionsByRequestedTime = noOfCases(impact.currentlyInfected, results);
+
+
+}
 
 export default covid19ImpactEstimator;
