3025a0e1b156c122956b2bee1efe911f57510800
wawerualbert58 <wawerualbert58@gmail.com>
2020-04-13 06:03:36 -0400

    spacing

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,10 @@
 const covid19ImpactEstimator = (data) => {
- const input = data;
- return {
-     data: input,
-     impact: {},
-     severeImpact: {}
- };
+  const input = data;
+  return {
+      data: input,
+      impact: {},
+      severeImpact: {}
+  };
 };
 
 export default covid19ImpactEstimator;
